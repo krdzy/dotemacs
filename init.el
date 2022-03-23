900989510eb1942c13cf49aceb3babf2d6b2e6da
Stefan Krdzavac <stefankrdzavac@gmail.com>
2022-03-23 15:51:49 +0100

    Inital emacs config commit.

---
+++ init.el
@@ -0,0 +1,21 @@
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "config/config.org"
+                   user-emacs-directory))
+
+
